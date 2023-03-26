f3169bf41a63a4e13c42bf5066c0d090f0c88467
Martin Laubscher <martin.laubscher@me.com>
2023-03-26 18:52:03 +0100

    bullets vanish after a bit

--- src/application/Bullet.java
+++ src/application/Bullet.java
@@ -4,30 +4,37 @@ import javafx.scene.shape.Polygon;
 
 public class Bullet extends Character {
     private static final int SPEED = 10;
-    private static final int MAXDIST = 400;
+    private static final double MAXDIST = 1000;
 
-//    x-coordinate
-    private int x;
-//    y-coordinate
-    private int y;
+//    x-coordinate of spawn point
+    private int xSpawn;
+//    y-coordinate of spawn point
+    private int ySpawn;
 //    distance traveled
-    private int dist;
+    private double dist;
 //    flag indicating if player's bullet
     private boolean friendly;
 
     public Bullet(int x, int y, boolean friendly) {
         super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
-        this.x = x;
-        this.y = y;
+        this.xSpawn = x;
+        this.ySpawn = y;
         this.dist = 0;
         this.friendly = friendly;
     }
 
-    public void setDist(int dist) {
-        this.dist = dist;
+    public void setDist() {
+        // double currentX = this.getCharacter().getTranslateX();
+        // double currentY = this.getCharacter().getTranslateY();
+        // this.dist = Math.sqrt(Math.pow(currentX - xSpawn, 2) + Math.pow(currentY - ySpawn, 2));
+        this.dist += SPEED;
     }
 
-    public int getDist() {
+    public double getDist() {
         return dist;
+    }
+
+    public static double getMAXDIST() {
+        return MAXDIST;
     }
 }
