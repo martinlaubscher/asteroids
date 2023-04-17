030ac655b1cac81bbcdac53e8c048306ab9889fa
ridhima-singh-dev <ridhimasingh217@gmail.com>
2023-04-17 16:51:39 +0100

    Changed Size model class

--- src/model/Size.java
+++ src/model/Size.java
@@ -1,7 +1,18 @@
 package model;
 
+// if adding new sizes, make sure to maintain the descending order, otherwise the splitAsteroids method in GameController won't work properly!
 public enum Size {
-	LARGE,
-	MEDIUM,
-	SMALL
+    LARGE(20),
+    MEDIUM(50),
+    SMALL(100);
+
+    private final int points;
+
+    Size(int points) {
+        this.points = points;
+    }
+
+    public int points() {
+        return points;
+    }
 }
